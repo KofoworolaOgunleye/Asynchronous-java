62600faac4c31c69bb401e53aa217d0a6f9ff334
kofoworola <kay16th@yahoo.com>
2024-11-11 10:52:42 +0000

    feat: prints Hello World to the console asynchronously after a delay of 2 secs

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CompletableFuture<Void> future = CompletableFuture.runAsync(()-> {
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                e.printStackTrace();
+            }
+            System.out.println("Hello world!");
+        }) ;
+        future.join();
     }
+
 }
