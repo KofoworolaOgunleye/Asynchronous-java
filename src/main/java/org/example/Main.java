858d08d0301f019656100fc0614761cab9041168
kofoworola <kay16th@yahoo.com>
2024-11-11 10:58:19 +0000

    feat: prints Hello and World concurrently

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,16 +5,40 @@ import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
-        CompletableFuture<Void> future = CompletableFuture.runAsync(()-> {
+//        CompletableFuture<Void> future = CompletableFuture.runAsync(()-> {
+//            try {
+//                TimeUnit.SECONDS.sleep(2);
+//            } catch (InterruptedException e) {
+//                Thread.currentThread().interrupt();
+//                e.printStackTrace();
+//            }
+//            System.out.println("Hello world!");
+//        }) ;
+//        future.join();
+
+        //task2
+        CompletableFuture<Void> hello = CompletableFuture.runAsync(()-> {
             try {
-                TimeUnit.SECONDS.sleep(2);
+                TimeUnit.SECONDS.sleep(3);
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
                 e.printStackTrace();
             }
-            System.out.println("Hello world!");
+            System.out.println("Hello");
         }) ;
-        future.join();
+
+        CompletableFuture<Void> world = CompletableFuture.runAsync(()-> {
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                e.printStackTrace();
+            }
+            System.out.println("World");
+        }) ;
+
+        CompletableFuture<Void> helloWorld = CompletableFuture.allOf(hello,world);
+        helloWorld.join();
     }
 
 }
