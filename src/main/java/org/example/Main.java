16e46cc37fef8ed190ac0f2b80d0345b9e3d0253
mfahxd <fahadbaig788@gmail.com>
2024-11-11 11:32:40 +0000

    feat: created a method to asynchronously calculate factorial of a given string

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,14 @@
 package org.example;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -70,39 +75,63 @@ public class Main {
 
 
         //task 4
-        Random random = new Random();
-        CompletableFuture<String> hello = CompletableFuture.supplyAsync(()-> {
-            int delay = random.nextInt(10)+1;
-            try {
-                TimeUnit.SECONDS.sleep(delay);
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-                e.printStackTrace();
-            }
-            return "Hello";
-        }) ;
+//        Random random = new Random();
+//        CompletableFuture<String> hello = CompletableFuture.supplyAsync(()-> {
+//            int delay = random.nextInt(10)+1;
+//            try {
+//                TimeUnit.SECONDS.sleep(delay);
+//            } catch (InterruptedException e) {
+//                Thread.currentThread().interrupt();
+//                e.printStackTrace();
+//            }
+//            return "Hello";
+//        }) ;
+//
+//        CompletableFuture<String> world = CompletableFuture.supplyAsync(()-> {
+//            int delay = random.nextInt(10)+1;
+//            try {
+//                TimeUnit.SECONDS.sleep(delay);
+//            } catch (InterruptedException e) {
+//                Thread.currentThread().interrupt();
+//                e.printStackTrace();
+//            }
+//            return "World";
+//        }) ;
+//
+//        CompletableFuture<String> helloWorld = hello.thenCombine(world, (h,w)->h +" "+ w)
+//                        .orTimeout(10, TimeUnit.SECONDS);
+//        helloWorld.thenAccept(System.out::println)
+//                        .exceptionally(ex -> {
+//                            if (ex.getCause() instanceof TimeoutException) {
+//                                System.out.println("Error: Time exceeded 10 seconds");
+//                            }
+//                            return null;
+//                        });
+//        helloWorld.join();
+//    }
 
-        CompletableFuture<String> world = CompletableFuture.supplyAsync(()-> {
-            int delay = random.nextInt(10)+1;
-            try {
-                TimeUnit.SECONDS.sleep(delay);
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-                e.printStackTrace();
-            }
-            return "World";
-        }) ;
+        String data = "85671 34262 92143 50984 24515 68356 77247 12348 56789 98760";
 
-        CompletableFuture<String> helloWorld = hello.thenCombine(world, (h,w)->h +" "+ w)
-                        .orTimeout(10, TimeUnit.SECONDS);
-        helloWorld.thenAccept(System.out::println)
-                        .exceptionally(ex -> {
-                            if (ex.getCause() instanceof TimeoutException) {
-                                System.out.println("Error: Time exceeded 10 seconds");
-                            }
-                            return null;
-                        });
-        helloWorld.join();
+        List<BigInteger> numbers = Arrays.stream(data.split(" "))
+                .map(BigInteger::new)
+                .collect(Collectors.toList());
+
+        CompletableFuture.supplyAsync(() -> numbers)
+                .thenApply(list ->
+                        list.stream()
+                                .map(Main::calculateFactorial)
+                                .collect(Collectors.toList())
+                )
+                .thenAccept(result ->
+                        System.out.println("Factorials: " + result)
+                ).join();
     }
 
+    private static BigInteger calculateFactorial(BigInteger num) {
+        BigInteger result = BigInteger.ONE;
+        for (BigInteger i = BigInteger.ONE; i.compareTo(num) <= 0; i = i.add(BigInteger.ONE)) {
+            result = result.multiply(i);
+        }
+        return result;
+    }
 }
